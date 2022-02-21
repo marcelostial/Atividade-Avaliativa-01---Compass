23371d5e82cb8639630dbabdc989f6cc39cdb0b1
hiimarcelo <marceloaface@outlook.com>
2022-02-21 10:59:54 -0300

    fix: corrigindo logica calculo imc

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
+
+
 function imcCalculationlogical(){
     const $ = document.querySelector.bind(document);
    
-    const peso = $("#displayPeso");
-    const altura  = $("#displayAltura");
-    const calcular = $("#inputCalcular");
+    let peso = $("#displayPeso");
+    let altura  = $("#displayAltura");
+    let calcular = $("#inputCalcular");
 
-    const imcCalculo = $("#calculoIMC");
-    const reposta  = $("#repostaIMC");
+    let imcCalculo = $("#calculoIMC");
+    let reposta  = $("#repostaIMC");
     
     calcular.addEventListener("click", () => { 
+        let imc = (peso.value) / (altura.value * altura.value).toFixed(1)
+        imcCalculo.textContent=`IMC: ${imc}`;
 
-
-    let imc = peso.value / (altura.value * altura.value);
-
-    imcCalculo.textContent=`IMC: ${imc.toFixed()}`;
-
-    console.log(peso.value)
-
-    if (imc < 18,5){
-        reposta.textContent=`(Abaixo do Peso)`; 
-        }
-
-    else if (imc > 18,5 && imc < 24,9){
-        reposta.textContent=`(Peso Normal)`; 
-        }
-
-    else if (imc > 25 && imc < 29,9){
-        reposta.textContent=`(Sobrepeso)`; 
-        }
-
-    else if (imc > 30 && imc < 34,9){
-        reposta.textContent=`(Obesidade Grau I)`; 
-        }  
+        if (imc < 18.5){
+            reposta.textContent=`(Abaixo do Peso)`; 
+            }
     
-    else if (imc > 35 && imc < 39,9){
-        reposta.textContent = `(Obesidade Grau II)`; 
-        }
+        else if (imc > 18.5 && imc < 24.9){
+            reposta.textContent=`(Peso Normal)`; 
+            }
     
-    else if (imc >= 40){
+        else if (imc > 25 && imc < 29.9){
+            reposta.textContent=`(Sobrepeso)`; 
+            }
+    
+        else if (imc > 30 && imc < 34.9){
+            reposta.textContent=`(Obesidade Grau I)`; 
+            }  
+        
+        else if (imc > 35 && imc < 39.9){
+            reposta.textContent = `(Obesidade Grau II)`; 
+            }
+        
+        else {
             reposta.textContent = `(Obesidade Mórbida)`; 
-        }   
-
-    })
+            }   
+    
+        })
 }
+
+imcCalculationlogical()
+
 
